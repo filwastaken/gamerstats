d80783e6d522e4674e4ba92d9b9e10ae2fd1a7b8
Filippo Olimpieri <olimpieri.1933529@studenti.uniroma1.it>
2023-05-23 16:47:36 +0200

    Popolazione db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,18 +20,24 @@ class PagesController < ApplicationController
 '''
 
     ids = []
-    for i in 800...4000
+    min = 5500
+    max = 6000
+
+    for i in min...max    #primo valore a 3313
       ids.push(i)
     end
     volte = 0
     ids.each do |id|
       if volte < ids.size
-        puts "#{volte}/#{ids.size}"
-        BattlenetOauthService.infoId(session[:access_token], id)
+        puts "#{volte}/#{ids.size}  => #{id}/#{max}"
+        #BattlenetOauthService.infoId(session[:access_token], id)
         #BattlenetOauthService.infoEroiId(session[:access_token], id)
       end
       volte = volte + 1
     end
+    #valori = Stat.pluck(:id) # Estrae tutti i valori della colonna dal database
+    #risultato = valori.map { |valore| "\"#{valore}\"" }.join(",") # Formatta i valori nel formato richiesto
+    #puts risultato
     puts "//////////////////"
 
   end
