0ced9841a5944de50a5a05ee104e8ff69f5cca98
Noemi2001 <giustini.1933541@studenti.uniroma1.it>
2023-06-20 17:28:26 +0200

    logout sistemato

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'sign_out', to: 'users#sign_out'
+  
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
 
   resources :home do
     member do
