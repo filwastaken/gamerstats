5f6ca265e4b6798afe706c1598ce1f7a1b5076fd
Noemi2001 <giustini.1933541@studenti.uniroma1.it>
2023-06-26 09:54:02 +0200

    campo playerId nella registrazione

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_21_151554) do
+ActiveRecord::Schema.define(version: 2023_06_26_065000) do
 
   create_table "teams", force: :cascade do |t|
     t.string "nome_team"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2023_06_21_151554) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role"
+    t.string "playerid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
