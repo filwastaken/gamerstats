3ee241d271f80b4090296814793f927e15f31f4a
Filippo Guerra <guerra.1931976@studenti.uniroma1.it>
2023-06-27 18:58:20 +0200

    OAuth e Signup debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
     protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:playerid])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:playerid])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:uid])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:uid])
     end
   
 end
--- db/migrate/20230626065000_add_player_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPlayerIdToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :playerid, :string
-  end
-end
