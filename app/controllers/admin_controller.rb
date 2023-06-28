21547050009a3447d7081606d72e40d5dc8fba59
Filippo Guerra <guerra.1931976@studenti.uniroma1.it>
2023-06-28 20:26:47 +0200

    - Added user and team tables in /admin/adminpage.html.erb - Created user_delete and team_delete functions (check controller and routes) - Removed puts from header

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,21 @@
 class AdminController < ApplicationController
-  before_action :set_stat, only: %i[ show edit update destroy ]
-
   # GET /admin or /admin.json
-  def index
+  def adminpage
+    @savedusers = User.all
+    @savedteams = Team.all
   end
+
+  # DELETE /admin#delete_team
+  def delete_team
+    Team.find(params[:id]).destroy
+    redirect_to "/adminpage"
+  end
+
+  # DELETE /admin#delete_user
+  def delete_user
+    User.find(params[:id]).destroy
+    redirect_to "/adminpage"
+  end
+
+  # // TODO: Regala abbonamento
 end
