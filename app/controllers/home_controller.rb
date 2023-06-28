645bcfbb1b447425a25e7cf3bd5cbf5cccd5987e
Filippo Olimpieri <olimpieri.1933529@studenti.uniroma1.it>
2023-06-28 09:40:19 +0200

    integrati processi in background dopo il merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,38 @@
 class HomeController < ApplicationController
   def index
+    #reset_session
+
+    ###PER FAR FUNZIONARE TUTTO BISOGNA SCRIVERE SUL TERMINALE :
+      #1) redis-server su un terminale
+      #2) bundle exec sidekiq su un altro terminale
+      #3) rails s su un terzo terminale
+
+    Sidekiq::Queue.all.each(&:clear)
+    Sidekiq::RetrySet.new.clear
+    Sidekiq::ScheduledSet.new.clear
+    Sidekiq::DeadSet.new.clear
+
+    a = 80
+    interval = 100
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "1")
+    a = a + interval
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "2")
+    a = a + interval
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "3")
+    a = a + interval
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "4")
+    a = a + interval
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "5")
+    a = a + interval
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "6")
+    a = a + interval
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "7")
+    a = a + interval
+    BackgroundJob.perform_async(session[:access_token], a, a+interval, "8")
+
+    #valori = Stat.pluck(:id) # Estrae tutti i valori della colonna dal database
+    #risultato = valori.map { |valore| "\"#{valore}\"" }.join(",") # Formatta i valori nel formato richiesto
+    #puts risultato
   end
 
   def personstats
