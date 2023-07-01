2233e846883f9dab66785418f1fad94a86a55c17
Filippo Olimpieri <olimpieri.1933529@studenti.uniroma1.it>
2023-07-01 16:15:10 +0200

    sistemata registrazione

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
     # app/controllers/application_controller.rb
-    protect_from_forgery with: :null_session
 
     before_action :configure_permitted_parameters, if: :devise_controller?
   
