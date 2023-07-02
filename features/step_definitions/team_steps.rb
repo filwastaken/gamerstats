869c1c9da5490ff2b65bf61bb93a350d4dbbb165
Filippo Olimpieri <olimpieri.1933529@studenti.uniroma1.it>
2023-07-02 18:56:26 +0200

    aggiunto test bottoni abbonamento e team leader

--- features/step_definitions/team_steps.rb
+++ features/step_definitions/team_steps.rb
@@ -5,13 +5,17 @@ Given("I am logged in as a user with uid {int}") do |uid|
     assert_not_nil user, "User with UID #{uid} not found"
   
     visit '/users/sign_in'
-    fill_in "user_email", :with => "pinco.pallino@gmail.com"
+    fill_in "user_email", :with => user.email
     fill_in "user_password", :with => "111111"
     click_button "Log in"
 end
 
 And("I am on the new team page") do 
     visit new_team_path
+end
+
+And("I am on the home page") do 
+    visit home_index_path
 end
 
 When("I fill in {string} with {string}") do |field, value|
