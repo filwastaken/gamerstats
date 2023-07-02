ef04ffc8066c767ff4dcf0196dea92f0d52937f8
Noemi2001 <giustini.1933541@studenti.uniroma1.it>
2023-07-02 21:16:07 +0200

    sistemato update team e team_stat

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -172,6 +172,32 @@ class TeamsController < ApplicationController
     @team1 = Team.new(team_params)
     if( @team1.giocatore2!="" || @team1.giocatore3!="" || @team1.giocatore4!="")
 
+      if Team.exists?(giocatore1: @team1.giocatore1, giocatore2: @team1.giocatore2, giocatore3: @team1.giocatore3, giocatore4: @team1.giocatore4)
+        flash[:notice] = "Esiste già un team con gli stessi giocatori!"
+        redirect_to edit_team_path
+        return
+      elsif Team.exists?(giocatore1: @team1.giocatore1, giocatore2: @team1.giocatore3, giocatore3: @team1.giocatore2, giocatore4: @team1.giocatore4)
+        flash[:notice] = "Esiste già un team con gli stessi giocatori!"
+        redirect_to edit_team_path
+        return
+      elsif Team.exists?(giocatore1: @team1.giocatore1, giocatore2: @team1.giocatore4, giocatore3: @team1.giocatore3, giocatore4: @team1.giocatore2)
+        flash[:notice] = "Esiste già un team con gli stessi giocatori!"
+        redirect_to edit_team_path
+        return
+      elsif Team.exists?(giocatore1: @team1.giocatore1, giocatore2: @team1.giocatore3, giocatore3: @team1.giocatore4, giocatore4: @team1.giocatore2)
+        flash[:notice] = "Esiste già un team con gli stessi giocatori!"
+        redirect_to edit_team_path
+        return
+      elsif Team.exists?(giocatore1: @team1.giocatore1, giocatore2: @team1.giocatore2, giocatore3: @team1.giocatore4, giocatore4: @team1.giocatore3)
+        flash[:notice] = "Esiste già un team con gli stessi giocatori!"
+        redirect_to edit_team_path
+        return
+      elsif Team.exists?(giocatore1: @team1.giocatore1, giocatore2: @team1.giocatore4, giocatore3: @team1.giocatore2, giocatore4: @team1.giocatore3)
+        flash[:notice] = "Esiste già un team con gli stessi giocatori!"
+        redirect_to edit_team_path
+        return
+      end
+
       if(Team.exists?(nome_team: @team1.nome_team) && @team1.nome_team != @team.nome_team)
         flash[:notice] = "Esiste già un team con lo stesso nome"
         redirect_to edit_team_path
@@ -179,16 +205,16 @@ class TeamsController < ApplicationController
       end
 
       giocatori = []
-      if(@team1.giocatore1 != nil)
+      if(@team1.giocatore1 != "")
         giocatori.append(@team1.giocatore1)
       end
-      if(@team1.giocatore2 != nil)
+      if(@team1.giocatore2 != "")
         giocatori.append(@team1.giocatore2)
       end
-      if(@team1.giocatore3 != nil)
+      if(@team1.giocatore3 != "")
         giocatori.append(@team1.giocatore3)
       end
-      if(@team1.giocatore4 != nil)
+      if(@team1.giocatore4 != "")
         giocatori.append(@team1.giocatore4)
       end
   
@@ -227,7 +253,7 @@ class TeamsController < ApplicationController
       end
       respond_to do |format|
         if @team.update(team_params)
-          calculate_averages(@team)
+          calculate_averages_update(@team)
           format.html { redirect_to team_url(@team), notice: "Team was successfully updated." }
           format.json { render :show, status: :ok, location: @team }
         else
@@ -239,6 +265,46 @@ class TeamsController < ApplicationController
       flash[:notice] = "Un team deve essere composto da almeno due giocatori!"
       redirect_to edit_team_path
     end
+  end
+
+  def calculate_averages_update(team)
+    player_ids = [team.giocatore1, team.giocatore2, team.giocatore3, team.giocatore4]
+    players_stats = Stat.where(uid: player_ids)
+    
+    averages = {}
+    averages[:terranwins] = players_stats.average(:terranwins)
+    averages[:protosswins] = players_stats.average(:protosswins)
+    averages[:zergwins] = players_stats.average(:zergwins)
+    averages[:careertotalgames] = players_stats.average(:careertotalgames)
+    averages[:totalwins] = players_stats.average(:totalwins)
+    averages[:totallosses] = players_stats.average(:totallosses)
+    averages[:wlratio] = players_stats.average(:wlratio)
+    averages[:level] = players_stats.average(:level)
+    averages[:levelterran] = players_stats.average(:levelterran)
+    averages[:totallevelxpterran] = players_stats.average(:totallevelxpterran)
+    averages[:currentlevelxpterran] = players_stats.average(:currentlevelxpterran)
+    averages[:levelzerg] = players_stats.average(:levelzerg)
+    averages[:totallevelxpzerg] = players_stats.average(:totallevelxpzerg)
+    averages[:currentlevelxpzerg] = players_stats.average(:currentlevelxpzerg)
+    averages[:levelprotoss] = players_stats.average(:levelprotoss)
+    averages[:totallevelxpprotoss] = players_stats.average(:totallevelxpprotoss)
+    averages[:currentlevelxpprotoss] = players_stats.average(:currentlevelxpprotoss)
+    averages[:wins1vs1] = players_stats.average(:wins1vs1)
+    averages[:games1vs1] = players_stats.average(:games1vs1)
+    averages[:wins2vs2] = players_stats.average(:wins2vs2)
+    averages[:games2vs2] = players_stats.average(:games2vs2)
+    averages[:wins3vs3] = players_stats.average(:wins3vs3)
+    averages[:games3vs3] = players_stats.average(:games3vs3)
+    averages[:wins4vs4] = players_stats.average(:wins4vs4)
+    averages[:games4vs4] = players_stats.average(:games4vs4)
+    averages[:winsarchon] = players_stats.average(:winsarchon)
+    averages[:gamesarchon] = players_stats.average(:gamesarchon)
+    averages[:totalpointsachievements] = players_stats.average(:totalpointsachievements)
+    averages[:seasontotalgames] = players_stats.average(:seasontotalgames)
+    averages[:totalgamesthisseason] = players_stats.average(:totalwinsthisseason)
+  
+    team_stat = TeamStat.find_by(team: team)
+    team_stat.update(averages)
   end
 
   # abbandona
