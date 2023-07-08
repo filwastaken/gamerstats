86734f7b76895a36f1a70ec61dbc7f5dfe003e30
Filippo Olimpieri <olimpieri.1933529@studenti.uniroma1.it>
2023-07-08 16:21:52 +0200

    risolto problema aggiunta statistiche

--- app/workers/background_job.rb
+++ app/workers/background_job.rb
@@ -1,3 +1,5 @@
+=begin
+
 class BackgroundJob
   include Sidekiq::Worker
 
@@ -10,43 +12,20 @@ class BackgroundJob
     puts "//////////////////"
   end
 end
+=end
 
 
 
-=begin
 class BackgroundJob
     include Sidekiq::Worker
   
-    def perform(access_token, min, max, thread, threads, intervallo)
+    def perform(access_token, min, max, thread)
       puts "//////////////////"
 
       for profileId in min..max
-        puts "#{profileId}      thread: #{thread}      #{profileId} / #{max}    CICLO 1"
+        puts "#{profileId}      thread: #{thread}      #{profileId} / #{max}"
         BattlenetOauthService.ottieniProfilo(access_token, profileId)
       end
-
-      #if(max+intervallo*threads < 2321)
-      SecondJob.perform_async(access_token, max+intervallo*threads, max+intervallo*threads+intervallo, thread, threads, intervallo)
-      #end  
       puts "//////////////////"
     end
 end
-
-class SecondJob
-  include Sidekiq::Worker
-
-  def perform(access_token, min, max, thread, threads, intervallo)
-    puts "//////////////////"
-
-      for profileId in min..max
-        puts "#{profileId}      thread: #{thread}      #{profileId} / #{max}       CICLO 2"
-        BattlenetOauthService.ottieniProfilo(access_token, profileId)
-      end
-
-    #if(max+intervallo*threads < 2321)
-    BackgroundJob.perform_async(access_token, max+intervallo*threads, max+intervallo*threads+intervallo, thread, threads, intervallo)
-    #end
-    puts "//////////////////"
-  end
-end
-=end
