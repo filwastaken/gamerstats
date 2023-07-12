16a22f9387fec369b2f401f67ffd943b5e10d1df
Filippo Guerra <guerra.1931976@studenti.uniroma1.it>
2023-07-12 18:17:13 +0200

    Removed comments, recreated server, upgraded cucumber and created a database backup

--- app/workers/background_job.rb
+++ app/workers/background_job.rb
@@ -1,27 +1,7 @@
-=begin
-
-class BackgroundJob
-  include Sidekiq::Worker
-
-  def perform(access_token, min, max, thread, threads, intervallo)
-    puts "//////////////////"
-    min.each do |profileId|
-      puts "#{profileId}      thread: #{thread}"
-      BattlenetOauthService.ottieniProfilo(access_token, profileId)
-    end
-    puts "//////////////////"
-  end
-end
-=end
-
-
-
 class BackgroundJob
     include Sidekiq::Worker
     def perform(access_token, min, max, thread)
-      puts "//////////////////"
       for profileId in min..max
-        puts "#{profileId}      thread: #{thread}      #{profileId} / #{max}"
         BattlenetOauthService.ottieniProfilo(access_token, profileId)
       end
       duplicates = Stat.group(:uid).having("COUNT(*) > 1")
@@ -29,6 +9,5 @@ class BackgroundJob
         duplicate_items = Stat.where(uid: duplicate.uid).order(:id)
         duplicate_items.offset(1).destroy_all
       end
-      puts "//////////////////"
     end
 end
